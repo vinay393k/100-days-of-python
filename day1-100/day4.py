a3a43e463283e45e34b7c1fcd8ebf8c85d2c2667
vinay <vinaykodipalli17@gmail.com>
2026-07-02 21:24:01 +0530

    add day 4 practice file

---
+++ day1-100/day4.py
@@ -0,0 +1,21 @@
+#swapping variables
+"""(a,b)=(5,10)
+a,b=b,a
+print(a,b)"""
+#counting
+"""word="vinay"
+length=(len(word))
+print(length)
+del word
+print(word)"""
+#object reference
+"""a=5
+b="vinay"
+c=a
+print(a,b,c)"""
+#invalid variable declaration
+'''if=17
+print(if)'''
+#taking input for user
+"""name=int(input(("enter ur name:")))
+print(name)"""
