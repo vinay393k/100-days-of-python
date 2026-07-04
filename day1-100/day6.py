ceb114d1e82b5120c3f141185007e230bc912a96
vinay <vinaykodipalli17@gmail.com>
2026-07-04 21:29:21 +0530

    adding day 6 progress

---
+++ day1-100/day6.py
@@ -0,0 +1,48 @@
+#to get all the keywords in python 
+"""import keyword
+print("list of keywords:")
+print(keyword.kwlist)"""
+
+"""for=10 #we not able to use keywords as a variable
+    print(for) # it returns invalid syntax error"""
+
+#python literals are mainly five types
+#numeric literals
+"""a=100
+b=-50
+c=3.14
+d=-0.004
+e=4+7j
+f=-3j
+print(a,b,c,d,e,f)"""
+
+#string literals
+"""a='vinay' #single quote string
+b="python"  #double quote string
+c='''learning python''' #triple quote string
+print(a,b,c)"""
+
+#boolean literals
+"""a=True
+b=False
+print(a)
+print(b)
+print(1==a) #here we get output as ture because 1 is also called ture
+print(0==b) #here it returns ture because 0 is also called false
+print(True+5)
+print(False+7)
+"""
+
+#collection literals
+"""list=[1,2,3] #store multiple values in specific order
+tuple=("tcs","google","hcltech","others") #we cannot modify after creation
+directory={"name":"vinay","age":"19","year":"3rd year"} #store data as key-value pair
+set={1,2,3,4,5} #store unique elements only
+print(list)
+print(tuple)
+print(directory)
+print(set)"""
+
+#special literals
+"""value=None #it is used for absence of a value or null values
+print(value)"""
