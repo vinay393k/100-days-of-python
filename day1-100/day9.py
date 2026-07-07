a568cf2adaa9feece1c6f742a1765dbebc63de16
vinay <vinaykodipalli17@gmail.com>
2026-07-07 20:11:30 +0530

    adding day 9 progress

---
+++ day1-100/day9.py
@@ -0,0 +1,18 @@
+#loops in python
+#for loop
+"""n=4
+for i in range (0,n):
+    print(i)"""
+
+#while loop
+"""n=0
+while(n<3):
+    n=n+1
+    print("hello world!")"""
+
+#nested loop
+"""for i in range(1,5):
+    for j in range(i):
+        print(i,end=' ')
+        print()
+"""
